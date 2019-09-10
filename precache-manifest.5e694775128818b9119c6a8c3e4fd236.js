25822a4de47931999e9bd8fd0d3a7a466d04c551
nicplackle <nicplackle@gmail.com>
2019-09-10 16:23:31 +0200

    Updates

---
+++ precache-manifest.5e694775128818b9119c6a8c3e4fd236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06520307d406f627a2924b2f461f9419",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "8e71d5e51b54c7049e1b",
+    "url": "/meme-generator/static/css/main.5c027ab7.chunk.css"
+  },
+  {
+    "revision": "c053ca14a20cdb81d201",
+    "url": "/meme-generator/static/js/2.280ca7d4.chunk.js"
+  },
+  {
+    "revision": "8e71d5e51b54c7049e1b",
+    "url": "/meme-generator/static/js/main.7fe6f953.chunk.js"
+  },
+  {
+    "revision": "19cad26144d8f0f77b1d",
+    "url": "/meme-generator/static/js/runtime~main.f5a09933.js"
+  }
+]);
